c5c6a28cf95948c6b731118fb598bf3af0d07426
varerysan <valery.mosyagin@gmail.com>
2018-10-20 15:21:41 +0300

    Add data output for c++ code

--- scripts/read_data.py
+++ scripts/read_data.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import csv
 #print ("hello")
 num = 0
@@ -5,29 +6,24 @@ sum = 0
 long = 7
 lat = 8
 
+max_number = 10
+
+print(max_number)
+
 with open('../data/file1.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in reader:
-        #print("---------------------------")
-        #print('\ndata: '.join(row))
         num += 1
         if num == 1:
             continue
 
         val_lat = int(row[lat])/1000/3600
         val_long = int(row[long])/1000/3600
-#        print("lat=", val_lat, " long=", val_long)
-        print("{ latitude: ",val_lat ,", longitude: ",val_long," },")
-#        print(val_lat,  val_long)
 
-#{ latitude: 56.249452, longitude: 43.883379 },
-#{ latitude: 56.247892, longitude: 43.884382 },
-#{ latitude: 56.248537, longitude: 43.880477 }
+#        print("{ latitude: ",val_lat ,", longitude: ",val_long," },")
+        print(val_lat,  val_long)
 
-        #print("num=", num, "val=", val)
-        #sum += val
-        if num > 500:
+
+        if num > max_number:
             break
 
-#print("n=",num)
-#print("mean=", sum/num)
